976d6e3e147ea77d757c7e66e36ce115d53a8428
withakay <jack@fader.co.uk>
2024-05-03 13:26:39 +0100

    first add

---
+++ extension.js
@@ -0,0 +1,66 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const vscode = require('vscode');
+
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "git-browse" is now active!');
+
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with  registerCommand
+	// The commandId parameter must match the command field in package.json
+	let disposable = vscode.commands.registerCommand('git-browse.open', function () {
+		// The code you place here will be executed every time your command is executed
+
+		// Display a message box to the user
+		OpenInBrowser
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+// This method is called when your extension is deactivated
+function deactivate() { }
+
+const regex = /(?:git@|https:\/\/)(github\.com|gitlab\.com|bitbucket\.org)[:\/]([a-zA-Z0-9_.\-]+\/[a-zA-Z0-9_.\-]+\.git)/;
+
+function extractUrl(text) {
+	const match = regex.exec(text);
+	if (match) {
+		const hostname = match[1];
+		const repoPath = match[2];
+		const httpsUrl = `https://${hostname}/${repoPath}`;
+		return httpsUrl;
+	} else {
+		return null;
+	}
+}
+
+async function OpenInBrowser() {
+	const editor = vscode.window.activeTextEditor;
+	const selection = editor.selection;
+	const lineRange = editor.document.lineAt(selection.active.line).range
+	const text = editor.document.getText(lineRange);
+
+	const url = extractUrl(text)
+	if (url) {
+		vscode.env.openExternal(vscode.Uri.parse(url));
+	}
+	else {
+		vscode.window.showInformationMessage('No Git URLs found.');
+	}
+}
+
+module.exports = {
+	extractUrl,
+	activate,
+	deactivate
+}
---
+++ test/extension.test.js
@@ -0,0 +1,28 @@
+const assert = require('assert');
+
+// You can import and use all API from the 'vscode' module
+// as well as import your extension to test it
+const vscode = require('vscode');
+// const myExtension = require('../extension');
+
+suite('Extension Test Suite', () => {
+	vscode.window.showInformationMessage('Start all tests.');
+
+	test('Extract Github SSH URL', () => {
+		const { extractUrl } = require('../extension');
+		const text = "Here is some Git repository SSH URL: git@github.com:username/repo.git : Let see if this is extracted";
+		assert.strictEqual(extractUrl(text), "https://github.com/username/repo.git");
+	});
+
+	test('Extract Gitlab SSH URL', () => {
+		const { extractUrl } = require('../extension');
+		const text = "Here is some Git repository SSH URL: git@gitlab.com:username/repo.git : Let see if this is extracted";
+		assert.strictEqual(extractUrl(text), "https://gitlab.com/username/repo.git");
+	});
+
+	test('Invalid URL', () => {
+		const { extractUrl } = require('../extension');
+		const text = "Here is some Git repository SSH URL: github.org:username/repo.git : Let see if this is extracted";
+		assert.strictEqual(extractUrl(text), null);
+	});
+});
